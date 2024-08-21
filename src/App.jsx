56b4d8bfe994b25ebb58c915d07a15bba00c67de
javid shabin <javid.main.act@gmail.com>
2024-08-21 13:37:40 +0530

    Create react app and add needed folder and home, signup, and login jsx file. Install tailwind, react hook form , axios, lucid icons, router, hot toast, then config the every dependisies

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router/routes";
+import { Toaster } from "react-hot-toast";
+
+function App() {
+  return <>
+  <RouterProvider router={router} />
+  <Toaster/>
+  </>;
+}
+
+export default App;
