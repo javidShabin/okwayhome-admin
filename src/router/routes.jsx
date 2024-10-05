0a3307ec3ebe10128cc48048c8257d89b949f8b1
javid shabin <javid.main.act@gmail.com>
2024-10-06 00:35:32 +0530

    Install axios and config the axios as axios inntents

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 
 
+
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <div>Home</div>
   },
 ]);
